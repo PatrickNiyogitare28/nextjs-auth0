2a09dcf0b49a4a42449a3ad7a5f159e7caae6462
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2021-08-21 02:57:55 +0200

    feat(auth) added auth0 authenitcation

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,6 +1,6 @@
 import auth0 from '../../utils/auth0';
-
-export default async function login(req, res) {
+console.log("env: ..... "+process.env.domain);
+export default async function login(req, res) { 
   try {
     await auth0.handleLogin(req, res, {});
   } catch (error) {
